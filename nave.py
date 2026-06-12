3662fb467ea5a46714bdf26b1839b615453402ed
begnami <caiobegnamii@gmail.com>
2026-06-12 08:39:53 -0300

    Adicionei a função de não permitir viajar sem nenhum passageiro

--- nave.py
+++ nave.py
@@ -3,11 +3,14 @@ tripulantes = []
 
 def viajar():
     global combustivel
-    if combustivel >= 30:
-        combustivel = combustivel - 30
-        print("A nave viajou")
+    if len(tripulantes) > 0:
+        if combustivel >= 30:
+            combustivel = combustivel - 30
+            print("A nave viajou")
+        else:
+            print("Você está sem combustível suficiente. Abasteça!")
     else:
-        print("Você está sem combustível suficiente. Abasteça!")
+        print("Sem tripulantes suficiente")
 
 def abastecer():
     global combustivel
@@ -25,13 +28,15 @@ def registrarT():
     print("Tripulante inserido com sucesso! 🚀")
 
 def tirarT():
-    tripulantes.pop()
-    print("Último tripulante retirado com sucesso!")
-
+    if len(tripulantes) > 0:
+        removido = tripulantes.pop()
+        print(f"🧑🏼‍🚀 {removido} foi removido da missão")
+    else:
+        print("Ninguém a bordo para remover")
 print("\n--- Nave da Bola ---")
 print("\nBem vindo ao menu interativo da nave. Por favor selecione uma opção: ")
 while True:
-    print("\n1- Mostrar status da nave | 2- Viajar | 3- Abastecer | 4- Novo tripulante | 5- Sair | 6- Tirar último tripulante")
+    print("\n1- Mostrar status da nave | 2- Viajar | 3- Abastecer | 4- Novo tripulante | 5- Tirar último tripulante | 6- Sair")
     opçao = input("Escolha: ")
     if (opçao == "1"):
         status()
@@ -42,12 +47,14 @@ while True:
     elif(opçao == "4"):
         registrarT()
     elif(opçao == "5"):
-        print("Viagem encerrada!")
-        break
-    elif(opçao =="6"):
         if len(tripulantes) == 0:
             print("Nenhum tripulante foi removido porque não há nenhum tripulante")
         else:
             tirarT()
+        
+    elif(opçao =="6"):
+        print("Viagem encerrada!")
+        break
+        
 
 ## lista.pop() Tira o ultimo elemento da lista
